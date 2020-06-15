1a304771fb06ee8d427e2baa652180188604a5f4
Lee Bush <leebush@leebush.com>
2020-06-14 21:54:31 -0700

    document and automate deployment process.

---
+++ scripts/build_and_release_to_pypi.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+# Before releasing:
+# * test & build.
+# * ensure version is bumped in `setup.py`.
+# * create a version tag. `git tag -a v-N.N.N`
+# run this script. ./scripts/build_and_release_to_pypi.sh
+
+set -e  # fail on first error
+
+rm -rf ./build
+rm -rf ./dist
+rm -rf ./venv
+python3.5 -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
+python3 -m pip install --user --upgrade setuptools wheel
+python3 setup.py sdist bdist_wheel
+python3 -m pip install --upgrade twine
+python3 -m twine upload --repository testpypi dist/*
+python3 -m twine upload --repository pypi dist/*
